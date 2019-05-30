975a557e464a2529ef16f68f3da8843785285b3b
david medina <davidmeddina@gmail.com>
2019-05-30 18:36:02 -0500

    Model associations done

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,2 +1,8 @@
 class Discussion < ApplicationRecord
+  belongs_to :user
+  belongs_to :channel
+  has_many :replies, dependent: :destroy
+
+  validates :title, :content, presence: :true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :discussions
+  resources :channels
+  resources :discussions do
+    resources :replies
+  end
+  
   root 'discussions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
