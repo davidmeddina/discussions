9b82d5d29dd90f7b1ba897f383293cf9e07f948f
david medina <davidmeddina@gmail.com>
2019-05-30 15:31:15 -0500

    Add discussions Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  resources :discussions
+  root 'discussions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   devise_for :users, controllers: {registrations: 'registrations'}
